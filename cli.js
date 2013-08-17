35558886250d17a7a2c2fc1fa5daeef0f4026ee0
Max Ogden <max@maxogden.com>
2013-08-16 18:24:16 -0700

    use cross platform EOLs

--- cli.js
+++ cli.js
@@ -1,39 +1,9 @@
 #!/usr/bin/env node
+var path = require('path')
+var dat = require(__dirname)(process.cwd())
+var cli = require(path.join(__dirname, 'lib', 'parse-cli'))
+var optimist = require('optimist')
+var EOL = require('os').EOL
 
-var dat = require('./')(process.cwd())
-
-var opts = require('optimist')
-  .usage("Usage: $0 <command> [<args>]\n\nEnter 'dat help' for help")
-
-parse(dat, opts)
-
-function parse(dat, opts) {
-  var args = opts.argv._
-  var error = validate(dat, args)
-  if (error) return process.stdout.write(error)
-  var cmd = commands(opts)
-  dat[cmd.command].call(dat, cmd.options, function(err) {
-    if (err) return console.error('fatal', err.message)
-  })
-}
-
-function validate(dat, args) {
-  var args = opts.argv._
-  if (args.length === 0) return opts.help()
-  if (!dat[args[0]]) {
-    return ['Invalid command: ' + args[0], opts.help()].join('\n')
-  }
-}
-
-function commands(opts) {
-  var args = opts.argv._
-  var cmd = args.shift()
-  var options = {}
-  args.map(function(arg) { options[arg] = true })
-  var skip = ['$0', '_']
-  Object.keys(opts.argv).map(function(arg) {
-    if (skip.indexOf(arg) > -1) return
-    options[arg] = opts.argv[arg]
-  })
-  return {command: cmd, options: options}
-}
+var opts = optimist.usage("Usage: $0 <command> [<args>]" + EOL + EOL + "Enter 'dat help' for help")
+cli.parse(dat, opts)
--- index.js
+++ index.js
@@ -12,18 +12,23 @@ function Dat(dir, opts) {
   this.opts = opts
 }
 
-Dat.prototype.exists = function(dir, cb) {
+Dat.prototype.exists = function(options, cb) {
+  var dir = process.cwd()
+  if (options.path) dir = options.path
   var datPath = path.join(dir, '.dat')
+  var levelPath = path.join(datPath, 'data.dat')
   fs.exists(datPath, function(exists) {
     if (!exists) return cb(false, false)
-    
+    fs.exists(levelPath, function(exists) {
+      cb(false, exists)
+    })
   })
 }
 
 Dat.prototype.init = function(dir) {
-  process.stdout.write('TODO\n')
+  console.log('TODO')
 }
 
 Dat.prototype.help = function() {
-  process.stdout.write('TODO\n')
+  console.log('TODO')
 }
