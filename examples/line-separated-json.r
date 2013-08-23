b382d0262630a93595b69ab116f1cc25308a05d0
Max Ogden <max@maxogden.com>
2013-08-23 14:02:58 -0700

    update r example transform

--- examples/line-separated-json.r
+++ examples/line-separated-json.r
@@ -1,4 +1,15 @@
 #!/usr/bin/env Rscript
+
+# a simple passthrough stream example in R
+# write one json object per line to stdin
+# this writes one json object per line to stdout
+
+# change this function to modify each R list object as it gets processed
+transform <- function(obj) {
+  # e.g. obj["foo"] = "bar"
+  return(obj)
+}
+
 require(RJSONIO)
 finished <- FALSE
 f <- file("stdin")
@@ -9,7 +20,12 @@ while (!finished) {
     finished <- TRUE
   } else {
     obj <- fromJSON(line)
-    print(typeof(obj))
-    print(obj)
+    obj <- transform(obj)
+    json <- gsub("\n", "", toJSON(obj))
+    cat(paste(json, "\n", sep=""))
   }
 }
+
+# notes
+# using cat() instead of print() keeps the json from getting escaped
+# to use: install the RJSONIO R package and have Rscript in your PATH
