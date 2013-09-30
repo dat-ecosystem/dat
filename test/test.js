2bbe350c10d3bd519e35a193070c8a8a4b36a994
Max Ogden <max@maxogden.com>
2013-09-30 16:34:59 -0700

    crashing test, need to implement raw data storage types

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ var os = require('os')
 var datPath = path.join(__dirname, '..')
 var Dat = require(datPath)
 var test = require('tape')
+var concat = require('concat-stream')
 var tmp = os.tmpdir()
 
 test('.paths', function(t) {
@@ -49,4 +50,34 @@ test('.init in existing repo', function(t) {
   })
 })
 
+test('creating single row with write stream', function(t) {
+  getDat(t, function(dat, done) {
+    var ws = dat.createWriteStream()
+    ws.write('foo')
+    ws.end()
+    ws.on('close', function() {
+      dat.storage.currentData().pipe(concat(function(data) {
+        console.log('data', data.toString())
+        done()
+      }))
+    })
+  })
+})
 
+function getDat(t, cb) {
+  var dat = new Dat(tmp)
+  dat.init(function(err, msg) {
+    t.false(err, 'no err')
+    dat._storage({}, function(err, seq) {
+      t.false(err, 'no err')
+      cb(dat, done)
+    })
+  })  
+  
+  function done() {
+    dat.destroy(function(err) {
+      t.false(err, 'no err')
+      t.end()
+    })
+  }
+}
