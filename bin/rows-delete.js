cb6a73d3ac4d069215376e17de4bf266861b19ee
Finn <derfinn@gmail.com>
2014-09-17 13:49:16 +0200

    remove unnecessary line

--- bin/rows-delete.js
+++ bin/rows-delete.js
@@ -1,7 +1,6 @@
 module.exports = function (dat, opts, cb) {
   var args = opts._.slice(2)
   if (args.length < 1) return cb(new Error('Usage: dat rows delete <rowKey>'))
-  var opts = {}
   
   dat.delete(args[0],function (err, newVersion) {
     if(err) return cb(err)
