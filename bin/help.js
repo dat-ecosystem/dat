6475100db2eb1f8ca63facbf99173e9960e2c4a7
Max Ogden <max@maxogden.com>
2015-02-25 21:22:59 -0800

    remove files from alpha (they are being re-implemented in dat-core now)

--- bin/help.js
+++
@@ -1,9 +0,0 @@
-var ansimd = require('ansimd')
-var fs = require('fs')
-var path = require('path')
-
-module.exports = function(dat, opts, cb) {
-  var help = fs.readFileSync(path.join(__dirname, '..', 'docs', 'cli-usage.md'))
-  console.log(ansimd(help))
-  process.nextTick(cb)
-}
--- test/tests/finish.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (test, cb) {
-  test('', function(t) {
-    if (cb) cb()
-    t.end()
-  })
-}
