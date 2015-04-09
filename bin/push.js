6480c51bc0d3475b1156e978b4674fadb50b9d4b
Karissa McKelvey <krmckelv@gmail.com>
2015-04-08 23:50:24 -0700

    Add pull, push to usage, add new cmds to root help

--- bin/push.js
+++ bin/push.js
@@ -1,3 +1,5 @@
+var fs = require('fs')
+var path = require('path')
 var url = require('url')
 var openDat = require('../lib/open-dat.js')
 var sshStream = require('../lib/ssh-stream.js')
@@ -8,11 +10,11 @@ module.exports = {
 }
 
 function handlePush (args) {
-  if (args._.length === 0) abort(new Error('Must pass an argument to dat push'))
+  if (args._.length === 0) return usage()
   var remote = args._[0]
   var parsed = url.parse(remote)
-  if (!parsed.protocol) abort(new Error('Invalid push URL'))
-  if (parsed.protocol !== 'ssh:') abort(new Error('Invalid push URL'))
+  if (!parsed.protocol) return usage()
+  if (parsed.protocol !== 'ssh:') return usage()
 
   openDat(args, function ready (err, db) {
     if (err) abort(err)
@@ -26,4 +28,8 @@ function abort (err, message) {
   if (message) console.error(message)
   if (err) throw err
   process.exit(1)
+}
+
+function usage () {
+  console.error(fs.readFileSync(path.join(__dirname, '..', 'usage', 'push.txt')).toString())
 }
