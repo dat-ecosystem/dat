e8014ed09b2cceb41625a4f058eb649d554a02ec
Karissa McKelvey <krmckelv@gmail.com>
2015-04-22 15:00:52 -0700

    Add preliinary manual merge function

--- bin/merge.js
+++ bin/merge.js
@@ -1,4 +1,8 @@
 var pump = require('pump')
+var knead = require('knead')
+var batcher = require('byte-stream')
+var through = require('through2')
+
 var usage = require('../lib/usage.js')('merge.txt')
 var abort = require('../lib/abort.js')
 var openDat = require('../lib/open-dat.js')
@@ -18,9 +22,29 @@ function handleMerge (args) {
   openDat(args, function ready (err, db) {
     if (err) return abort(err)
     var mergeStream = db.merge(headA, headB)
-    pump(process.stdin, mergeStream, function done (err) {
+
+    var resolutionStream
+
+    if (args.live) {
+      resolutionStream = process.stdin
+    } else {
+      var diffStream = db.createDiffStream(headA, headB)
+
+      var limit = 20 * 2 // rows * 2 (to include diffs)
+      var batchStream = batcher(limit)
+      var opts = {
+        rowPath: function (row) { return row.value }
+      }
+      resolutionStream = diffStream.pipe(batchStream).pipe(knead(opts))
+      resolutionStream.on('data', function (data) {
+        console.log('resolutionStream', data)
+      })
+    }
+
+    pump(resolutionStream, mergeStream, function done (err) {
       if (err) return abort(err)
       console.error('Merged', headA, headB, 'into', db.head)
     })
   })
 }
+
