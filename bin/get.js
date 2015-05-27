488bf0c8c93cad5e9081cbdc01d692639095ebc4
Karissa McKelvey <krmckelv@gmail.com>
2015-05-27 15:43:56 -0700

    dat get <key> as per cli doc, add tests

--- bin/get.js
+++ bin/get.js
@@ -14,73 +14,27 @@ module.exports = {
   command: handleRows,
   options: [
     {
-      name: 'format',
-      boolean: false,
-      abbr: 'f'
-    },
-    {
       name: 'dataset',
       boolean: false,
       abbr: 'd'
-    },
-    {
-      name: 'greater-than-equal',
-      boolean: false,
-      abbr: 'gte'
-    },
-    {
-      name: 'greater-than',
-      boolean: false,
-      abbr: 'gt'
-    },
-    {
-      name: 'less-than-equal',
-      boolean: false,
-      abbr: 'lte'
-    },
-    {
-      name: 'less-than',
-      boolean: false,
-      abbr: 'lt'
-    },
-    {
-      name: 'limit',
-      boolean: false,
-      abbr: 'l'
     }
   ]
 }
 
 function handleRows (args) {
   debug('handleRows', args)
-  if (args.help) return usage()
+  if (args.help || !args.dataset || args._.length === 0) {
+    usage()
+    abort()
+  }
 
   openDat(args, function ready (err, db) {
     if (err) abort(err)
     var key = args._[0]
-    if (!args.f) args.f = 'ndjson'
-    if (args.f === 'json') args.f = 'ndjson'
 
-    var parseReadStream
-    if (args.f === 'ndjson') {
-      parseReadStream = ndjson.serialize()
-    } else {
-      parseReadStream = through.obj(function (data, enc, next) {
-        var val = data.value
-        val.key = data.key
-        next(null, val)
-      })
-    }
-
-    if (!key) {
-      pump(db.createReadStream(args), parseReadStream, formatData(args.f), process.stdout, function done (err) {
-        if (err) abort(err, 'dat get error')
-      })
-    } else {
-      db.get(key, args, function (err, value) {
-        if (err) abort(err, 'dat get error')
-        process.stdout.write(JSON.stringify(value))
-      })
-    }
+    db.get(key, args, function (err, value) {
+      if (err) abort(err, 'dat get error')
+      process.stdout.write(JSON.stringify(value))
+    })
   })
 }
--- tests/get.js
+++ tests/get.js
@@ -1,4 +1,5 @@
 var os = require('os')
+var fs = require('fs')
 var path = require('path')
 var test = require('tape')
 var spawn = require('tape-spawn')
@@ -18,37 +19,6 @@ test('dat import dataset', function (t) {
   st.end()
 })
 
-test('dat get from dataset', function (t) {
-  var st = spawn(t, dat + ' get --dataset=get-test', {cwd: dat1})
-  st.stderr.empty()
-  st.stdout.match(function (output) {
-    var lines = output.split('\n')
-    if (lines.length === 10) {
-      var line = JSON.parse(lines[0])
-      if (line.key === 'ak11246285') {
-        return line.value.latitude === '61.3482'
-      }
-      return true
-    }
-  })
-  st.end()
-})
-
-test('dat get from dataset with csv', function (t) {
-  var st = spawn(t, dat + ' get --dataset=get-test --format=csv', {cwd: dat1})
-  st.stderr.empty()
-  st.stdout.match(function (output) {
-    var lines = output.split('\n')
-    if (lines.length === 11) { // 1 header row + 10 lines
-      var headers = lines[0].split(',')
-      t.equals(headers.length, 16)
-      return true
-    }
-    return false
-  })
-  st.end()
-})
-
 test('dat get a key from dataset', function (t) {
   var st = spawn(t, dat + ' get ak11246293 --dataset=get-test', {cwd: dat1})
   st.stderr.empty()
@@ -57,5 +27,26 @@ test('dat get a key from dataset', function (t) {
     if (output.key === 'ak11246293' && output.value.latitude === '60.0366') return true
     return false
   })
+  st.end()
+})
+
+test('dat get without key errors', function (t) {
+  var st = spawn(t, dat + ' get --dataset=get-test', {cwd: dat1})
+  st.stdout.empty()
+  st.stderr.match(fs.readFileSync(path.join('usage', 'get.txt')).toString() + '\n', 'usage matched')
+  st.end()
+})
+
+test('dat get without dataset errors', function (t) {
+  var st = spawn(t, dat + ' get ak11246293', {cwd: dat1})
+  st.stdout.empty()
+  st.stderr.match(fs.readFileSync(path.join('usage', 'get.txt')).toString() + '\n', 'usage matched')
+  st.end()
+})
+
+test('dat get without key and dataset errors', function (t) {
+  var st = spawn(t, dat + ' get', {cwd: dat1})
+  st.stdout.empty()
+  st.stderr.match(fs.readFileSync(path.join('usage', 'get.txt')).toString() + '\n', 'usage matched')
   st.end()
 })
