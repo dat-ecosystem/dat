6f8db4580deb37b3004bbf3aba0fcf817ae51661
Karissa McKelvey <krmckelv@gmail.com>
2015-06-09 00:18:17 -0700

    Add test for grabing remote from dat.json

--- tests/push-pull-clone.js
+++ tests/push-pull-clone.js
@@ -1,4 +1,5 @@
 var os = require('os')
+var fs = require('fs')
 var path = require('path')
 var test = require('tape')
 var spawn = require('tape-spawn')
@@ -11,6 +12,7 @@ var csv = path.resolve(__dirname + '/fixtures/all_hour.csv')
 
 helpers.onedat(dat1)
 
+// test clone
 test('push-pull-clone: dat import csv', function (t) {
   var st = spawn(t, dat + ' import ' + csv + ' -d quakes', {cwd: dat1})
   st.stderr.match(/Done importing data/)
@@ -25,7 +27,33 @@ test('push-pull-clone: clone dat1', function (t) {
   st.end()
 })
 
-test('push-pull-clone: compare statuses', function (t) {
+test('push-pull-clone: compare statuses after clone', function (t) {
+  compareStatuses(t, dat1, dat2)
+})
+
+// test pull from remote
+test('push-pull-clone: dat import dataset #2 to dat1', function (t) {
+  var st = spawn(t, dat + ' import ' + csv + ' -d quakes-2', {cwd: dat1})
+  st.stderr.match(/Done importing data/)
+  st.stdout.empty()
+  st.end()
+})
+
+test('push-pull-clone: pull from dat1 to dat2 with remote set in dat.json', function (t) {
+  var config = {remote: dat1}
+  fs.writeFileSync(path.join(dat2, 'dat.json'), JSON.stringify(config), 'utf8')
+  var st = spawn(t, dat + ' pull', {cwd: dat2})
+  st.stderr.match(/Pulled/)
+  st.stdout.empty()
+  st.end()
+})
+
+test('push-pull-clone: compare statuses after pull', function (t) {
+  compareStatuses(t, dat1, dat2)
+})
+
+function compareStatuses (t, dat1, dat2) {
+  // get dat1 'dat status' json
   var st1 = spawn(t, dat + ' status --json', {cwd: dat1, end: false})
   var status1
 
@@ -48,8 +76,6 @@ test('push-pull-clone: compare statuses', function (t) {
         return status1 === output
       } catch (e) {}
     }, 'status1 matches status2')
-
     st2.end()
   })
-
-})
+}
