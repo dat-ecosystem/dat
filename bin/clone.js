79b74e22e06c619baebe1d6a05046cd30a72f932
Mathias Buus <mathiasbuus@gmail.com>
2015-07-04 00:01:07 +0200

    fix clone progress message

--- bin/clone.js
+++ bin/clone.js
@@ -44,5 +44,5 @@ function handleClone (args) {
     else console.error('Clone from remote to %s has completed.', path)
   })
 
-  progress(cloneStream, {verb: 'Cloned', replicate: true})
+  progress(cloneStream, {verb: 'Cloning', replicate: true})
 }
