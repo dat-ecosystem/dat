ecb68fafd0cd481858caadd86cec7eebd82856b9
Karissa McKelvey <krmckelv@gmail.com>
2015-07-08 15:40:10 -0400

    Revert "Enable an alternative backend in dat configuration"
    This reverts commit 08f9b2ce49c8616a87e8c9de13fd3b5e5e27fc5b.

--- bin/init.js
+++ bin/init.js
@@ -1,3 +1,4 @@
+var path = require('path')
 var abort = require('../lib/util/abort.js')
 var init = require('../lib/util/init-dat.js')
 var usage = require('../lib/util/usage.js')('init.txt')
@@ -12,14 +13,13 @@ function handleInit (args) {
   init(args, function (err, results, db) {
     if (err) return abort(err, args)
     var msg
-    var datPath = args.path
     if (results.exists) {
-      msg = 'Skipping init, there is already a dat at ' + datPath
+      msg = 'Skipping init, there is already a dat at ' + path.join(args.path, '.dat')
       if (args.json) console.error({message: msg, exists: true})
       else console.error(msg)
       process.exit(0)
     } else if (results.created) {
-      msg = 'Initialized a new dat at ' + datPath
+      msg = 'Initialized a new dat at ' + path.join(args.path, '.dat')
       if (args.json) console.error({message: msg, created: true})
       else console.error(msg)
       process.exit(0)
