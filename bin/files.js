feb141a7b6684f578cf6edcf6a125385c9a04662
Karissa McKelvey <krmckelv@gmail.com>
2015-09-15 15:37:01 -0700

    modularize file lister code

--- bin/files.js
+++ bin/files.js
@@ -1,9 +1,8 @@
-var through = require('through2')
 var pump = require('pump')
-var formatData = require('format-data')
+var listFilesStream = require('../lib/files.js')
 var openDat = require('../lib/util/open-dat.js')
-var abort = require('../lib/util/abort.js')
 var usage = require('../lib/util/usage.js')('files.txt')
+var abort = require('../lib/util/abort.js')
 
 module.exports = {
   name: 'files',
@@ -40,33 +39,18 @@ module.exports = {
 function handleFiles (args) {
   if (args.help) return usage()
 
+  var limit = args.limit
+  if (limit) {
+    if (args.limit) args.limit = parseInt(limit, 10)
+    if (isNaN(args.limit)) abort(new Error('invalid limit: ' + limit), args)
+  }
+
+  args.dataset = 'files'
+
   openDat(args, function (err, db) {
     if (err) abort(err, args)
 
-    var limit = args.limit
-    if (limit) {
-      if (args.limit) args.limit = parseInt(limit, 10)
-      if (isNaN(args.limit)) abort(new Error('invalid limit: ' + limit), args)
-    }
-
-    args.dataset = 'files'
-
-    var stream = db.createKeyStream(args)
-    var formatter
-    if (args.json) {
-      formatter = formatData({
-        format: 'json',
-        style: 'object',
-        key: 'files',
-        suffix: '}\n'
-      })
-    } else {
-      formatter = through.obj(function (obj, enc, next) {
-        next(null, obj + '\n')
-      })
-    }
-
-    pump(stream, formatter, process.stdout, function (err) {
+    pump(listFilesStream(db, args), process.stdout, function (err) {
       if (err) abort(err, args, 'Error getting file list')
       db.close()
     })
