6fafd1bbd3a16cc3bce3718088f863761da737a0
Karissa McKelvey <krmckelv@gmail.com>
2016-02-26 19:50:53 -0800

    remove the else

--- fs.js
+++ fs.js
@@ -40,23 +40,21 @@ module.exports.createDownloadStream = function (archive, stats, opts) {
   if (!opts) opts = {}
 
   var downloader = through.obj(function (item, enc, next) {
-    if (opts.files && (opts.files.indexOf(item.name)) === -1) next()
-    else {
-      var downloadStats = archive.download(item, function (err) {
-        if (err) return next(err)
-        if (item.type === 'directory') stats.progress.directories++
-        else stats.progress.filesRead++
-        next()
+    if (opts.files && (opts.files.indexOf(item.name)) === -1) return next()
+    var downloadStats = archive.download(item, function (err) {
+      if (err) return next(err)
+      if (item.type === 'directory') stats.progress.directories++
+      else stats.progress.filesRead++
+      next()
+    })
+    if (item.type === 'file') {
+      stats.fileQueue.push({
+        name: item.name,
+        stats: downloadStats
       })
-      if (item.type === 'file') {
-        stats.fileQueue.push({
-          name: item.name,
-          stats: downloadStats
-        })
-        downloadStats.on('ready', function () {
-          stats.progress.bytesRead += downloadStats.bytesInitial
-        })
-      }
+      downloadStats.on('ready', function () {
+        stats.progress.bytesRead += downloadStats.bytesInitial
+      })
     }
   })
   downloader.stats = stats
