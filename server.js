c9501c7965eab076d25d94258e865cd2dd5ee432
karissa <krmckelv@gmail.com>
2016-04-05 17:54:18 -0700

    add missing code to make the daemon work

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ var dat = require('.')()
 module.exports = function (server, stream) {
   return {
     join: function (id, cb) {
+      var archive = dat.drive.get(id, '.')
       dat.join(id)
-      console.log('join', id)
+      var stream = archive.createEntryStream()
+      stream.resume()
+      stream.on('data', function (data) {
+        console.log('got item', data)
+        archive.download(data, function (err) {
+          if (err) throw err
+          console.log('downloaded')
+        })
+      })
       cb()
     },
     leave: function (id, cb) {
