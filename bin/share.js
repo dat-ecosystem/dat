0f9d75fc512e2c4bf81a95414c120373f31781aa
karissa <krmckelv@gmail.com>
2016-06-20 18:32:00 -0700

    create yolowatch module

--- bin/share.js
+++ bin/share.js
@@ -2,24 +2,22 @@ var hyperdrive = require('hyperdrive')
 var walker = require('folder-walker')
 var each = require('stream-each')
 var raf = require('random-access-file')
-var fs = require('fs')
 var path = require('path')
 var chalk = require('chalk')
 var prettyBytes = require('pretty-bytes')
 var speedometer = require('speedometer')
 var createSwarm = require('hyperdrive-archive-swarm')
+var yoloWatch = require('yolowatch')
 var statusLogger = require('../lib/status-logger')
 var swarmLogger = require('../lib/swarm-logger')
 
 module.exports = function (args) {
   var db = args.level
-  var dir = args.dir
-  var pathName = dir === '.' ? process.cwd() : dir
+  var dir = args.dir === '.' ? process.cwd() : args.dir
   var key = args.key ? Buffer(args.key, 'hex') : null
   var drive = hyperdrive(db)
   var swarm = null
   var logger = statusLogger(args)
-  var firstAppend = true
   var noDataTimeout = null
   var stats = {
     filesTotal: 0,
@@ -29,7 +27,7 @@ module.exports = function (args) {
   }
 
   var archive = drive.createArchive(key, {
-    name: path.basename(pathName), // TODO: hyperdrive support for this
+    name: path.basename(dir), // TODO: hyperdrive support for this
     live: !args.snapshot,
     file: function (name) {
       return raf(path.join(dir, name), {readable: true, writable: false})
@@ -45,7 +43,7 @@ module.exports = function (args) {
       process.exit(0)
     }
 
-    logger.message('Initializing Dat in ' + pathName)
+    logger.message('Initializing Dat in ' + dir)
 
     logger.status('', 0) // reserve total progress and size
     if (key) logger.status(printDatLink(archive.key), 1)
@@ -56,7 +54,7 @@ module.exports = function (args) {
       if (!key) db.put('!dat!key', archive.key.toString('hex'))
       logger.status(printDatLink(archive.key), 1)
       swarm = createSwarm(archive, args)
-      swarmLogger(swarm, logger, 'Sharing ' + pathName)
+      swarmLogger(swarm, logger, 'Sharing ' + dir)
     }
 
     archive.on('upload', function (data) {
@@ -92,10 +90,6 @@ module.exports = function (args) {
   }
 
   function checkAppend (data, next) {
-    if (firstAppend) {
-      firstAppend = false // folder walker seems off on the first item. TODO: investigate
-      return next()
-    }
     archive.lookup(data.relname, function (err, result) {
       if (!err && result) {
         var msg = chalk.green.dim('[DONE] ') + chalk.dim(result.name)
@@ -109,10 +103,6 @@ module.exports = function (args) {
   }
 
   function appendEntry (data, next) {
-    if (firstAppend) {
-      firstAppend = false // folder walker seems off on the first item. TODO: investigate
-      return next()
-    }
     archive.append({type: data.type, name: data.relname}, function () {
       var msg = chalk.green.dim('[DONE] ') + chalk.dim(data.relname)
       if (data.type === 'file') msg += chalk.dim(' (' + prettyBytes(data.stat.size) + ')')
@@ -144,14 +134,15 @@ module.exports = function (args) {
       if (args.snapshot) {
         logger.status(printDatLink(archive.key), 1)
         swarm = createSwarm(archive, args)
-        swarmLogger(swarm, logger, 'Sharing Snapshot ' + pathName)
+        swarmLogger(swarm, logger, 'Sharing Snapshot ' + dir)
         return
       }
 
-      yoloWatch(dir, function (name, st) {
-        if (name.indexOf('.dat') > -1) return
-        archive.append({type: st.isDirectory() ? 'directory' : 'file', name: name}, function () {
-          logger.message(chalk.green.dim('[Added] ') + chalk.dim(name))
+      var watcher = yoloWatch(dir)
+      watcher.on('data', function (file) {
+        if (file.filepath.indexOf('.dat') > -1) return
+        archive.append({type: file.type, name: file.relname}, function () {
+          logger.message(chalk.green.dim('[Added] ') + chalk.dim(file.relname))
           logger.status('', 0)
           printTotalStats()
         })
@@ -162,39 +153,5 @@ module.exports = function (args) {
   function onerror (err) {
     console.error(err.stack || err)
     process.exit(1)
-  }
-
-  function yoloWatch (dir, onchange) {
-    var stats = {}
-
-    kick(true, function () {
-      fs.watch(dir, function () {
-        kick(false, function () {})
-      })
-    })
-
-    function kick (first, cb) {
-      fs.readdir(dir, function (err, files) {
-        if (err) return
-
-        loop()
-
-        function loop () {
-          var file = files.shift()
-          if (!file) return cb()
-
-          fs.stat(path.join(dir, file), function (err, st) {
-            if (err) return loop()
-
-            if (!stats[file] || st.nlink !== stats[file].nlink) {
-              stats[file] = st
-              if (!first) onchange(file, st)
-            }
-
-            loop()
-          })
-        }
-      })
-    }
   }
 }
