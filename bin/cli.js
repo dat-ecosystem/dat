ef77dab958574b99313aa084b0c711c67399272a
Joe Hand <joe@hand.email>
2017-04-27 17:14:52 -0700

    update sync commands

--- bin/cli.js
+++ bin/cli.js
@@ -71,39 +71,31 @@ function syncShorthand (opts) {
   debug('Sync shortcut command')
   debug(opts)
 
-  console.error('TODO: use regular commands for now')
-  process.exit(1)
+  if (opts._.length > 1) {
+    // dat <link> {dir} - clone/resume <link> in {dir}
+    try {
+      debug('Clone sync')
+      opts.key = encoding.toStr(opts._[0])
+      opts.dir = opts._[1]
+      require('../lib/commands/clone').command(opts)
+    } catch (e) { return done() }
+  } else {
+    // dat {dir} - sync existing dat in {dir}
+    try {
+      debug('Share sync')
+      opts.dir = opts._[0]
+      fs.stat(opts.dir, function (err, stat) {
+        if (err || !stat.isDirectory()) return usage(opts)
 
-  // if (opts._.length > 1) {
-  //   // dat <link> {dir} - clone/resume <link> in {dir}
-  //   try {
-  //     debug('Clone sync')
-  //     opts.key = encoding.toStr(opts._[0])
-  //     opts.dir = opts._[1]
-  //     // make dir & start download
-  //     debug('mkdirp', opts.dir)
-  //     // TODO: do I want to mkdirp? or only one child?
-  //     mkdirp(opts.dir, function () {
-  //       require('../lib/download')('sync', opts)
-  //     })
-  //   } catch (e) { return done() }
-  // } else {
-  //   // dat {dir} - sync existing dat in {dir}
-  //   try {
-  //     debug('Share sync')
-  //     opts.dir = opts._[0]
-  //     fs.stat(opts.dir, function (err, stat) {
-  //       if (err || !stat.isDirectory()) return usage(opts)
+        // Set default opts. TODO: use default opts in sync
+        opts.watch = opts.watch || true
+        opts.import = opts.import || true
+        require('../lib/commands/sync').command(opts)
+      })
+    } catch (e) { return done() }
+  }
 
-  //       // Set default opts. TODO: use default opts in sync
-  //       opts.watch = opts.watch || true
-  //       opts.import = opts.import || true
-  //       require('../lib/commands/sync').command(opts)
-  //     })
-  //   } catch (e) { return done() }
-  // }
-
-  // function done () {
-  //   return usage(opts)
-  // }
+  function done () {
+    return usage(opts)
+  }
 }
