01714014a48c382e67998f939ac02e9a1448808b
Joe Hand <joe@joeahand.com>
2017-05-10 16:31:55 -0700

    add peer debug info (#745)
    * add peer debug info
    
    * standard
    
    * peers => sources

--- src/lib/network.js
+++ src/lib/network.js
@@ -1,3 +1,4 @@
+var speed = require('speedometer')
 var xtend = Object.assign
 
 module.exports = trackNetwork
@@ -13,13 +14,14 @@ function trackNetwork (state, bus) {
     state.network = xtend(network, state.network)
     bus.emit('dat:network')
 
-    network.on('connection', function (peer) {
+    network.on('connection', function (conn, info) {
       bus.emit('render')
-      peer.on('close', function () {
+      conn.on('close', function () {
         bus.emit('render')
       })
     })
 
+    if (state.opts.sources) trackSources()
     if (state.stats) return trackSpeed()
     bus.once('dat:stats', trackSpeed)
 
@@ -27,6 +29,42 @@ function trackNetwork (state, bus) {
       setInterval(function () {
         bus.emit('render')
       }, state.opts.logspeed)
+    }
+
+    function trackSources () {
+      state.sources = state.sources || {}
+      network.on('connection', function (conn, info) {
+        var id = info.id.toString('hex')
+        var peerSpeed = speed()
+
+        state.sources[id] = info
+        state.sources[id].speed = peerSpeed()
+        state.sources[id].getProgress = function () {
+
+          // TODO: how to get right peer from archive.content?
+          // var remote = conn.feeds[1].remoteLength
+          // // state.dat.archive.content.sources[0].feed.id.toString('hex')
+          // if (!remote) return
+          // return remote / dat.archive.content.length
+        }
+
+        conn.feeds.map(function (feed) {
+          feed.stream.on('data', function (data) {
+            state.sources[id].speed = peerSpeed(data.length)
+            bus.emit('render')
+          })
+          feed.stream.on('error', function (err) {
+            state.sources[id].error = err
+          })
+        })
+        bus.emit('render')
+
+        conn.on('close', function () {
+          state.sources[id].speed = 0
+          state.sources[id].closed = true
+          bus.emit('render')
+        })
+      })
     }
   }
 }
---
+++ src/ui/components/sources.js
@@ -0,0 +1,35 @@
+var output = require('neat-log/output')
+var pretty = require('prettier-bytes')
+var makeBar = require('progress-string')
+
+module.exports = peersUI
+
+function peersUI (state) {
+  if (!state.network) return ''
+  if (Object.keys(state.sources).length === 0) return ''
+
+  var peers = state.sources
+  // var stats = state.stats
+  // var peerCount = stats.peers.total || 0
+  // var complete = stats.peers.complete
+  var info = Object.keys(peers).map(function (id, i) {
+    return peerUI(peers[id], i)
+  }).join('\n')
+
+  return `\n${info}\n`
+
+  function peerUI (peer, i) {
+    var progress = peer.getProgress()
+    var bar = makeBar({
+      total: 100,
+      style: function (a, b) {
+        return `[${a}${b}] ${(progress).toFixed(2)}%`
+      }
+    })
+    var theBar = progress ? bar(progress) : '' // progress bar todo
+    return output`
+      [${i}] ${peer.closed ? 'CLOSED' : peer.type}: ${peer.host}:${peer.port} ${pretty(peer.speed)}/s
+      ${peer.error ? peer.error : theBar}
+    `
+  }
+}
