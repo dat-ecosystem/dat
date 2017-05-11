41b366a8cb895f228c7ee835d277cb7a959e29f2
Joe Hand <joe@hand.email>
2017-05-11 12:09:51 -0700

    add footer

--- src/lib/serve-http.js
+++ src/lib/serve-http.js
@@ -5,7 +5,9 @@ module.exports = runHttp
 
 function runHttp (state, bus) {
   var port = (typeof state.opts.http === 'boolean') ? 8080 : state.opts.http
-  var server = http.createServer(serve(state.dat.archive, {live: true}))
+  var footer = 'Served via Dat.'
+  // TODO: opts.exposeHeaders = true? Also exposes key which we mat not always want.
+  var server = http.createServer(serve(state.dat.archive, {live: true, footer: footer}))
   server.listen(port)
   server.on('listening', function () {
     state.http = { port: port, listening: true }
