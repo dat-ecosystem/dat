247cb772cf9388aa2a8b8dd881e39bf982110f85
Karissa McKelvey <karissa@users.noreply.github.com>
2017-06-27 13:43:45 -0700

    Publish metadata (#812)
    * Bump dat-registry minimum to fix bug when publishing to custom registries
    
    * make sure the dat joins the network when publishing
    
    * Create better error message when publish can't find the other computer
    
    * fix jwt error, closes #791
    
    * error for non existing archive

--- src/commands/publish.js
+++ src/commands/publish.js
@@ -51,8 +51,10 @@ function publish (opts) {
 
   opts.createIfMissing = false // publish must always be a resumed archive
   Dat(opts.dir, opts, function (err, dat) {
-    if (err) return exitErr(err)
-    // TODO better error msg for non-existing archive
+    if (err && err.name === 'MissingError') return exitErr('No existing dat in this directory. Create a dat before publishing.')
+    else if (err) return exitErr(err)
+
+    dat.joinNetwork() // join network to upload metadata
 
     var datjson = DatJson(dat.archive, {file: path.join(dat.path, 'dat.json')})
     datjson.read(publish)
@@ -94,13 +96,21 @@ function publish (opts) {
     }
 
     function makeRequest (datInfo) {
-      console.log(`'${chalk.bold(datInfo.name)}' will soon be ready for its great unveiling.`)
+      console.log(`Please wait, '${chalk.bold(datInfo.name)}' will soon be ready for its great unveiling...`)
       client.dats.create(datInfo, function (err, resp, body) {
         if (err) {
-          if (err.message) return exitErr('ERROR: ' + err.message) // node error
+          if (err.message) {
+            if (err.message === 'timed out') {
+              return exitErr(output`${chalk.red('\nERROR: ' + opts.server + ' could not connect to your computer.')}
+              Troubleshoot here: ${chalk.green('https://docs.datproject.org/troubleshooting#networking-issues')}
+              `)
+            }
+            var str = err.message.trim()
+            if (str === 'jwt expired') return exitErr(`Session expired, please ${chalk.green('dat login')} again`)
+            return exitErr('ERROR: ' + err.message) // node error
+          }
 
           // server response errors
-          if (err.toString().trim() === 'jwt expired') return exitErr(`Session expired, please ${chalk.green('dat login')} again`)
           return exitErr('ERROR: ' + err.toString())
         }
         if (body.statusCode === 400) return exitErr(new Error(body.message))
