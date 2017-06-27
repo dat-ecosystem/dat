7164a451c7586ed58783cf14deac0ed9c89d4efe
Joe Hand <joe@hand.email>
2017-06-27 14:13:01 -0700

    use dat-node directly for http

--- src/lib/serve-http.js
+++ src/lib/serve-http.js
@@ -1,16 +1,17 @@
-var http = require('http')
-var serve = require('hyperdrive-http')
 
 module.exports = runHttp
 
 function runHttp (state, bus) {
-  var port = (typeof state.opts.http === 'boolean') ? 8080 : state.opts.http
-  var footer = 'Served via Dat.'
-  // TODO: opts.exposeHeaders = true? Also exposes key which we mat not always want.
-  var server = http.createServer(serve(state.dat.archive, {live: true, footer: footer}))
-  server.listen(port)
-  server.on('listening', function () {
-    state.http = { port: port, listening: true }
-    bus.emit('render')
-  })
+  if (state.dat) return serve()
+  bus.once('dat', serve)
+
+  function serve () {
+    var port = (typeof state.opts.http === 'boolean') ? 8080 : state.opts.http
+    var server = state.dat.serveHttp({ port: port })
+
+    server.on('listening', function () {
+      state.http = { port: port, listening: true }
+      bus.emit('render')
+    })
+  }
 }
