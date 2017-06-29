5ae9220077dd9ce03f04779272c401d3ef509c23
Joe Hand <joe@hand.email>
2017-06-29 10:21:59 -0700

    test non-default http port =)

--- test/http.js
+++ test/http.js
@@ -35,3 +35,32 @@ test('http - share with http', function (t) {
   st.stderr.empty()
   st.end()
 })
+
+test('http - share with http other port', function (t) {
+  rimraf.sync(path.join(fixtures, '.dat'))
+  var cmd = dat + ' share --http 3333'
+  var st = spawn(t, cmd, {cwd: fixtures})
+
+  st.stdout.match(function (output) {
+    var sharingHttp = output.indexOf('Serving files over http') > -1
+    if (!sharingHttp) return false
+
+    request('http://localhost:3333', function (err, resp, body) {
+      t.error(err, 'no error')
+      t.ok(resp.statusCode === 200, 'okay status')
+      t.ok(body)
+
+      request('http://localhost:3333/folder/nested/hello.txt', function (err, resp, body) {
+        t.error(err, 'no error')
+        t.ok(resp.statusCode === 200, 'okay status')
+        t.same(body, 'code for science and society', 'body of file okay')
+
+        st.kill()
+      })
+    })
+
+    return true
+  })
+  st.stderr.empty()
+  st.end()
+})
