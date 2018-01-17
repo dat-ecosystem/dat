cdee0dbe66e5777f5ef9d5c469533e7fc36ec1ac
Max Ogden <max@maxogden.com>
2018-01-17 12:48:45 -0800

    generate installable zips

---
+++ package.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+# couldnt figure out undocumented 'output template' mode for pkg so wrote this
+# also need to include .node files until pkg supports including them in binary
+
+NODE_ABI="node-57"
+VERSION=$(node -pe "require('./package.json').version")
+
+rm -rf dist
+
+mkdir dist
+mkdir builds/dat-$VERSION-linux-x64
+mkdir builds/dat-$VERSION-macos-x64
+mkdir builds/dat-$VERSION-win-x64
+
+mv builds/dat-linux builds/dat-$VERSION-linux-x64/dat
+mv builds/dat-macos builds/dat-$VERSION-macos-x64/dat
+mv builds/dat-win.exe builds/dat-$VERSION-win-x64/dat.exe
+
+cp node_modules/utp-native/prebuilds/linux-x64/$NODE_ABI.node builds/dat-$VERSION-linux-x64/
+cp node_modules/utp-native/prebuilds/darwin-x64/$NODE_ABI.node builds/dat-$VERSION-macos-x64/
+cp node_modules/utp-native/prebuilds/win32-x64/$NODE_ABI.node builds/dat-$VERSION-win-x64/
+
+cp LICENSE builds/dat-$VERSION-linux-x64/
+cp LICENSE builds/dat-$VERSION-macos-x64/
+cp LICENSE builds/dat-$VERSION-win-x64/
+
+cp INSTALL builds/dat-$VERSION-linux-x64/README
+cp INSTALL builds/dat-$VERSION-macos-x64/README
+cp INSTALL builds/dat-$VERSION-win-x64/README
+
+cd builds
+../node_modules/.bin/cross-zip dat-$VERSION-linux-x64 ../dist/dat-$VERSION-linux-x64.zip
+../node_modules/.bin/cross-zip dat-$VERSION-macos-x64 ../dist/dat-$VERSION-macos-x64.zip
+../node_modules/.bin/cross-zip dat-$VERSION-win-x64 ../dist/dat-$VERSION-win-x64.zip
+
+rm -rf builds
+
+# now travis will upload the 3 zips in dist to the release
