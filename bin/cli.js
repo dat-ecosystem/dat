628304c2ca8e03a7fd9493fc3575604e3495bcf5
Joe Hand <joe@hand.email>
2018-03-27 11:19:50 -0700

    use update notifier for upgrade message

--- bin/cli.js
+++ bin/cli.js
@@ -3,7 +3,7 @@
 var subcommand = require('subcommand')
 var debug = require('debug')('dat')
 var usage = require('../src/usage')
-var versionCheck = require('../src/version-check')
+var pkg = require('../package.json')
 
 process.title = 'dat'
 
@@ -12,6 +12,21 @@ var NODE_VERSION_SUPPORTED = 4
 var nodeMajorVer = process.version.match(/^v([0-9]+)\./)[1]
 var invalidNode = nodeMajorVer < NODE_VERSION_SUPPORTED
 if (invalidNode) exitInvalidNode()
+else {
+  var notifier = require('update-notifier')
+  notifier({pkg: pkg})
+    .notify({
+      defer: true,
+      isGlobal: true,
+      boxenOpts: {
+        align: 'left',
+        borderColor: 'green',
+        borderStyle: 'classic',
+        padding: 1,
+        margin: {top: 1, bottom: 1}
+      }
+    })
+}
 
 if (debug.enabled) {
   debug('Dat DEBUG mode engaged, enabling quiet mode')
@@ -73,7 +88,6 @@ var config = {
 }
 
 if (debug.enabled) {
-  var pkg = require('../package.json')
   debug('dat', pkg.version)
   debug('node', process.version)
 }
@@ -84,7 +98,6 @@ match(alias(process.argv.slice(2)))
 
 function alias (argv) {
   var cmd = argv[0]
-  versionCheck()
   if (!config.aliases[cmd]) return argv
   argv[0] = config.aliases[cmd]
   return argv
