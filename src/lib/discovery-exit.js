b2cf4f12ceacc032f8540b1d16764f586d0e0ee4
okdistribute <633012+okdistribute@users.noreply.github.com>
2020-03-23 18:16:48 -0700

    use hyperswarm

--- src/lib/discovery-exit.js
+++ src/lib/discovery-exit.js
@@ -6,7 +6,7 @@ function discoveryExit (state, bus) {
   bus.once('network:callback', checkExit)
 
   function checkExit () {
-    if (state.dat.network.connected || !state.opts.exit) return
+    if (state.dat.network.connections || !state.opts.exit) return
     if (state.dat.network.connecting) return setTimeout(checkExit, 500) // wait to see if any connections resolve
     var msg = output(`
       Dat could not find any connections for that link.
